85c55b66bdb8be81fba16b6eea648ec0410d3c19
Mariusz Macheta <Mariusz.Macheta@gmail.com>
2016-04-25 21:04:42 +0200

    Missing file

---
+++ DDDCinema/DDDCinema.DataAccess/DbSetup/EntityMappings/MovieMapping.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using DDDCinema.Promotions;
+
+namespace DDDCinema.DataAccess.DbSetup.EntityMappings
+{
+	public class MovieMapping : ComplexTypeConfiguration<Movie>
+	{
+
+	}
+}

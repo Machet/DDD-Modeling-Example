b5446b23b3dce35f8877bb544943878aa8055f42
Mariusz Macheta <Mariusz.Macheta@gmail.com>
2016-04-28 00:49:14 +0200

    Fixing bugs

--- DDDCinema/DDDCinema/Controllers/NotificationController.cs
+++ DDDCinema/DDDCinema/Controllers/NotificationController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Web.Mvc;
 using DDDCinema.Application.Presentation.Audit;
+using DDDCinema.Common;
 
 namespace DDDCinema.Controllers
 {
@@ -8,16 +9,18 @@ namespace DDDCinema.Controllers
 	public class NotificationController : Controller
 	{
 		private readonly INotificationViewRepository _notificationRepository;
+		private readonly ICurrentUserProvider _userProvider;
 
-		public NotificationController(INotificationViewRepository notificationRepository)
+		public NotificationController(INotificationViewRepository notificationRepository, ICurrentUserProvider userProvider)
 		{
 			_notificationRepository = notificationRepository;
+			_userProvider = userProvider;
 		}
 
 		[HttpGet]
 		public ActionResult Index()
 		{
-			var data = _notificationRepository.GetNotificationsForUser(Guid.Parse(User.Identity.Name));
+			var data = _notificationRepository.GetNotificationsForUser(_userProvider.GetUserId().Value);
 			return View(data);
 		}
 	}
